276c73cc3513d64a82c291949c3abcaf3523764c
Rachman Aldiansyah <rachmanaldiansyah03@gmail.com>
2020-10-16 16:04:29 +0700

    Upload Project

---
+++ src/main/java/com/rsoftware/if1/latihan10/IF110119038Latihan10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rsoftware.if1.latihan10;
+
+/**
+ *
+ * @author
+ * NAMA  : Rachman Aldiansyah
+ * KELAS : IF1
+ * NIM   : 10119038
+ * Deskripsi Program : program ini berisi untuk menentukan status dewasa dengan fungsi logika
+ * 
+ */
+
+public class IF110119038Latihan10Logika {
+    
+    public static void main(String[] args) {
+        
+        int umur = 19;
+        boolean dewasa;
+        
+        dewasa = (umur >= 7);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+}
